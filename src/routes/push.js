9b9f413f54f44299e493ba2eecaa6549848b833e
Bastiaan Tiobook <deknudtb@gmail.com>
2021-08-23 22:52:00 +0200

    moved Pusher creation

--- src/routes/push.js
+++ src/routes/push.js
@@ -5,15 +5,17 @@ import Pusher from 'pusher';
 import { PUSHER_KEY, PUSHER_CLUSTER } from '$lib/Env';
 const { PUSHER_SECRET, PUSHER_APPID } = process.env;
 
-const pusher = new Pusher({
-  appId: PUSHER_APPID,
-  key: PUSHER_KEY,
-  secret: PUSHER_SECRET,
-  cluster: PUSHER_CLUSTER,
-  useTLS: true,
-});
 
 export async function get(){
+
+  const pusher = new Pusher({
+    appId: PUSHER_APPID,
+    key: PUSHER_KEY,
+    secret: PUSHER_SECRET,
+    cluster: PUSHER_CLUSTER,
+    useTLS: true,
+  });
+
   const newVal = Math.round(Date.now()/500%100);
   console.log(newVal);
   pusher.trigger(`counter`, 'next', newVal);
